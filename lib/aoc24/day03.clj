5d34770d93801f675804bdefa22523790868e60a
Lucas Lopes Paszinski <lucaspaszinski@gmail.com>
2024-12-03 19:11:05 -0300

    Improve day 03

--- lib/aoc24/day03.clj
+++ lib/aoc24/day03.clj
@@ -1,19 +1,24 @@
 (ns aoc24.day03)
 
+(defn mul [expr]
+  (->> expr
+       (re-seq #"\d{1,3}")
+       (map parse-long)
+       (reduce *)))
+
 (defn part1 [file]
   (->> (re-seq #"mul\(\d{1,3},\d{1,3}\)" (slurp file))
-       (map #(re-seq #"\d{1,3}" %))
-       (map (fn [[n1 n2]] (* (parse-long n1) (parse-long n2))))
+       (map mul)
        (reduce +)))
 
 (defn part2 [file]
   (->> (re-seq #"(mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\))" (slurp file))
        (map first)
-       (reduce (fn [[acc enabled?] itm]
-                 (cond (= itm "do()") [acc true]
-                       (= itm "don't()") [acc false]
-                       enabled? (let [[n1 n2] (re-seq  #"\d{1,3}" itm)]
-                                  [(+ acc (* (parse-long n1) (parse-long n2))) enabled?])
-                       :else [acc enabled?]))
-               [0 true])
+       (reduce
+        (fn [[acc enabled?] itm]
+          (cond (= itm "do()") [acc true]
+                (= itm "don't()") [acc false]
+                enabled? [(+ acc (mul itm)) enabled?]
+                :else [acc enabled?]))
+        [0 true])
        (first)))
