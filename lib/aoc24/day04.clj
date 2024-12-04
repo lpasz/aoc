71d7cc66f8bc839c3cc68d14f12f2a111ba74664
Lucas Lopes Paszinski <lucaspaszinski@gmail.com>
2024-12-04 07:21:38 -0300

    Day 04 part 2???

--- lib/aoc24/day04.clj
+++ lib/aoc24/day04.clj
@@ -2,9 +2,6 @@
   (:require [clojure.string :as s]
             [core :as c]))
 
-(def example "./assets/day04/example.txt")
-(def file "./assets/day04/input.txt")
-
 (defn xmas-dirs [[x y]]
   [[[x y] [(+ x 1) y] [(+ x 2) y] [(+ x 3) y]]
    [[x y] [(- x 1) y] [(- x 2) y] [(- x 3) y]]
@@ -15,15 +12,35 @@
    [[x y] [(+ x 1) (- y 1)] [(+ x 2) (- y 2)] [(+ x 3) (- y 3)]]
    [[x y] [(- x 1) (+ y 1)] [(- x 2) (+ y 2)] [(- x 3) (+ y 3)]]])
 
-(let [mtx (->> (s/split (slurp file) #"\n")
-               (map #(re-seq #"." %))
-               (c/to-matrix))]
-  (->> mtx
-       (mapcat (fn [[coord _]]
-                 (->> coord
-                      (xmas-dirs)
-                      (map (fn [coords] (reduce str (map mtx coords)))))))
-       (filter #{"XMAS" "SAMX"})
-       (count)))
+(defn part1 [file]
+  (let [mtx (c/to-matrix (slurp file))]
+    (->> mtx
+         (mapcat (fn [[coord _]]
+                   (->> coord
+                        (xmas-dirs)
+                        (map (fn [coords] (reduce str (map mtx coords)))))))
+         (filter #{"XMAS" "SAMX"})
+         (count)
+         ;; it double because it scans all twice to cover all directions
+         (#(/ % 2)))))
 
-(/ 4854 2)
+(defn x-mas [[x y]]
+  [[(inc x) (inc y)]
+   [(dec x) (dec y)]
+   [(dec x) (inc y)]
+   [(inc x) (dec y)]
+   [x y]])
+
+(defn part2 [file]
+  (let [mtx (c/to-matrix (slurp file))]
+    (->> mtx
+         (keep (fn [[coord letter]]
+                 (when (= letter \A)
+                   (->> coord
+                        (x-mas)
+                        (map mtx)
+                        (frequencies)))))
+
+         (filter #{{\M 2 \S 2 \A 1}})
+         (count))))
+
--- test/aoc24/day04_test.cljc
+++ test/aoc24/day04_test.cljc
@@ -4,13 +4,13 @@
 
 (t/deftest part1
   (t/testing "input"
-    (t/is (= (day04/part1 "./assets/day04/input.txt") 179571322)))
+    (t/is (= (day04/part1 "./assets/day04/input.txt") 2427)))
   (t/testing "example"
-    (t/is (= (day04/part1 "./assets/day04/example.txt") 161))))
+    (t/is (= (day04/part1 "./assets/day04/example.txt") 18))))
 
 (t/deftest part2
   (t/testing "input"
-    (t/is (= (day04/part2 "./assets/day04/input.txt") 103811193)))
+    (t/is (= (day04/part2 "./assets/day04/input.txt") 1957)))
   (t/testing "example"
-    (t/is (= (day04/part2 "./assets/day04/example2.txt") 48))))
+    (t/is (= (day04/part2 "./assets/day04/example.txt") 9))))
 
