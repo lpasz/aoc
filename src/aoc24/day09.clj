ed790c6877387f82ebad9f172563908a7b3c0639
Lucas Lopes Paszinski <lucaspaszinski@gmail.com>
2024-12-09 13:12:40 -0300

    Some improvements

--- src/aoc24/day09.clj
+++ src/aoc24/day09.clj
@@ -21,22 +21,50 @@
   (->> (re-seq #"." (slurp file))
        (map parse-long)
        (partition 2 2 [0])
-       (map-indexed (fn [idx [used free]]
-                      [(take used (repeat idx))
-                       (take free (repeat \.))]))
+       (map-indexed (fn [idx [used free]] [(take used (repeat idx)) (take free (repeat \.))]))
        (flatten)
        (vec)
        (free-space-right)
        (c/reject #(= \. %))
        (map-indexed #(* %1 %2))
-       (c/sum)
-       ;;
-       ))
-(part1 "./assets/day09/example.txt")
+       (c/sum)))
+
+(defn free-space-in-blocks-right [acc]
+  (loop [itms (reverse acc)
+         acc acc]
+    (if (empty? itms)
+      acc
+      (let [[mv-id mv-used] (first itms)
+            acc (->> acc
+                     (reduce (fn [[acc moved] [id used free]]
+                               (cond
+                         ; if already been moved, just free the space
+                                 (and moved (= mv-id id))
+                                 [(conj acc [\. 0 (+ free used)]) true]
+                         ; if has not moved so far, moves to the same space
+                                 (and (not moved) (= mv-id id))
+                                 [(conj acc [id used free]) true]
+                         ; if not yet moved and found a valid space move to that space
+                                 (and (not moved) (not= id mv-id) (>= free mv-used))
+                                 [(conj acc [id used 0] [mv-id mv-used (- free mv-used)]) true]
+                         ; otherwise just keep going
+                                 :else
+                                 [(conj acc [id used free]) moved]))
+                             [[] false])
+                     (first))]
+        (recur (rest itms) acc)))))
 
 (defn part2 [file]
   (->> (re-seq #"." (slurp file))
        (map parse-long)
        (partition 2 2 [0])
+       (map-indexed (fn [idx [used free]] [idx used free]))
+       (vec)
+       (free-space-in-blocks-right)
+       (map (fn [[id used free]] [(take used (repeat id)) (take free (repeat \.))]))
+       (flatten)
        (map-indexed vector)
-       ))
+       (c/reject #(= \. (second %)))
+       (map (fn [[a b]] (* a b)))
+       (c/sum)))
+
