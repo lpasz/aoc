71c5b914f5893cd573b2747641540caefb012486
Lucas Lopes Paszinski <lucaspaszinski@gmail.com>
2024-12-11 07:28:21 -0300

    Day 11 Part 1 and Part 2

--- src/aoc24/day11.clj
+++ src/aoc24/day11.clj
@@ -1,6 +1,46 @@
 (ns aoc24.day11
   (:require [core :as c]))
 
-(defn part1 [file] :not-implemented)
+(defn split-even-stone [stone len]
+  (let [half (quot len 2)]
+    (->> stone
+         (split-at half)
+         (map #(apply str %))
+         (map parse-long))))
 
-(defn part2 [file] :not-implemented)
+(defn change-stone [stone]
+  (let [str-stone (str stone)
+        str-stone-cnt (count str-stone)]
+    (cond (zero? stone) [1]
+          (even? (count str-stone)) (split-even-stone str-stone str-stone-cnt)
+          :else [(* 2024 stone)])))
+
+(defn zip-stones [stones]
+  (reduce (fn [acc stone]
+            (update
+             acc
+             stone
+             #(inc (or % 0))))
+
+          {} stones))
+(defn apply-blinks [file blinks]
+  (let [input (->> (re-seq #"\d+" (slurp file))
+                   (map parse-long)
+                   (zip-stones))]
+    (loop [blinks blinks stones input]
+      (if (zero? blinks)
+        (c/sum (vals stones))
+        (recur (dec blinks)
+               (reduce (fn [acc [old-stone cnt]]
+                         (reduce (fn [acc new-stone]
+                                   (update acc new-stone #(+ cnt (or % 0))))
+                                 acc
+                                 (change-stone old-stone)))
+                       {}
+                       stones))))))
+(defn part1 [file]
+  (apply-blinks file 25))
+
+(defn part2 [file]
+  (apply-blinks file 75))
+
--- test/aoc24/day11_test.cljc
+++ test/aoc24/day11_test.cljc
@@ -3,10 +3,10 @@
             [aoc24.day11 :as day11]))
 
 (t/deftest part1
-  (t/testing "input" (t/is (= (day11/part1 "./assets/day11/input.txt") nil)))
-  (t/testing "example" (t/is (= (day11/part1 "./assets/day11/example.txt") nil))))
+  (t/testing "input" (t/is (= (day11/part1 "./assets/day11/input.txt") 217812)))
+  (t/testing "example" (t/is (= (day11/part1 "./assets/day11/example.txt") 55312))))
 
 (t/deftest part2
-  (t/testing "input" (t/is (= (day11/part2 "./assets/day11/input.txt") nil)))
-  (t/testing "example" (t/is (= (day11/part2 "./assets/day11/example.txt") nil))))
+  (t/testing "input" (t/is (= (day11/part2 "./assets/day11/input.txt") 259112729857522)))
+  (t/testing "example" (t/is (= (day11/part2 "./assets/day11/example.txt") 65601038650482))))
 
