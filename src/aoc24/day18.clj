a49d6c7f6742fab4fccbd517e970e815d0a46276
Lucas Lopes Paszinski <lucaspaszinski@gmail.com>
2024-12-18 11:06:08 -0300

    Generic solver for day 18 part 1 and part 2

--- src/aoc24/day18.clj
+++ src/aoc24/day18.clj
@@ -1,25 +1,29 @@
 (ns aoc24.day18
   (:require [core :as c]))
 
-(defn after-n-bytes-shortest-path [n-bytes]
-  (let [nums (c/extract-numbers (slurp "./assets/day18/input.txt"))
+(defn after-n-bytes-shortest-path [file n-bytes [x y :as size]]
+  (let [nums (c/extract-numbers (slurp file))
         pairs (take n-bytes (partition 2 nums))
-        max-x (->> pairs (sort-by first >) (ffirst))
-        max-y (->> pairs (sort-by second >) (first) (second))
-        mtx (c/create-matrix 71 71 \.)
+        mtx (c/create-matrix (inc x) (inc y) \.)
         mtx (c/add-to-matrix mtx pairs \#)
         graph (c/matrix-to-graph mtx)
-        dijk   (c/dijkstra-shortest graph [0 0] [max-x max-y])]
-    [(last pairs) (get dijk [max-x max-y])]))
+        dijk  (c/dijkstra-shortest graph [0 0] size)]
+    [(last pairs) (get dijk size)]))
 
-(defn part1 []
-  (second (after-n-bytes-shortest-path 1024)))
+(defn part1 [file size after-n-bytes]
+  (second (after-n-bytes-shortest-path file after-n-bytes size)))
 
-(defn part2 []
-  ;; reduced via binary search manually done.
-  (->> (range 2950 3000)
-       (keep (fn [n] (let [[pair dist] (after-n-bytes-shortest-path n)]
-                       (when (infinite? dist)
-                         [n pair]))))
-       (first)
-       (second)))
+(defn part2 [file size start-byte]
+  (let [nums (c/extract-numbers (slurp file))]
+    (loop [rng (range start-byte (count nums))]
+      (let [[fh sh] (split-at (quot (count rng) 2) rng)
+            shh (first sh)
+            sht (rest sh)
+            [_coord1 dist1] (after-n-bytes-shortest-path file shh size)
+            [coord2 dist2] (after-n-bytes-shortest-path file (inc shh) size)
+            where [(infinite? dist1) (infinite? dist2)]]
+        (case where
+          [true true] (recur fh)
+          [false false] (recur sht)
+          [false true] coord2)))))
+
--- test/aoc24/day18_test.cljc
+++ test/aoc24/day18_test.cljc
@@ -3,8 +3,11 @@
             [aoc24.day18 :as day18]))
 
 (t/deftest part1
-  (t/testing "input" (t/is (= (day18/part1) 262))))
+  (t/testing "input" (t/is (= (day18/part1 "./assets/day18/input.txt" [70 70] 1024) 262)))
+  (t/testing "example" (t/is (= (day18/part1 "./assets/day18/example.txt" [6 6] 12) 22))))
 
 (t/deftest part2
-  (t/testing "input" (t/is (= (day18/part2) '(22 20)))))
+  (t/testing "input" (t/is (= (day18/part2 "./assets/day18/input.txt" [70 70] 1024) '(22 20))))
+  (t/testing "example" (t/is (= (day18/part2 "./assets/day18/example.txt" [6 6] 12) '(6 1)))))
+
 
