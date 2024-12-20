b2f05565183a47a6d004ac5a7e8bc1f4b5241559
Lucas Lopes Paszinski <lucaspaszinski@gmail.com>
2024-12-20 16:13:08 -0300

    Reuse Part 1 and Part 2

--- src/aoc24/day20.clj
+++ src/aoc24/day20.clj
@@ -1,56 +1,59 @@
 (ns aoc24.day20
   (:require [core :as c]))
 
-(defn part1 [file] :not-implemented)
+(defn possible-cheats
+  ([k]
+   (->> (c/cross k 2)
+        (map (fn [[p1 _ p2]] [p1 2 p2]))))
+  ([k ks]
+   (->> ks
+        (filter #(not= k %))
+        (keep #(let [md (c/manhattan-distance k %)]
+                 (when (<= md 20)
+                   [k md %]))))))
 
-(defn part2 [file] :not-implemented)
+(defn valid-cheats [dijk end possible-cheats]
+  (->> possible-cheats
+       (keep (fn [[c1 md c3]]
+               (let [v1 (dijk c1)
+                     v3 (dijk c3)]
+                 (when (and (number? v1) (number? v3)
+                            (> v3 v1))
+                   (let [n (if (< (dijk end) v3)
+                             (- (dijk end)
+                                (+ (- v3 (dijk end)) v1 md))
+                             (- v3 v1 md))]
+                     (when (pos? n) [#{c1 c3} n]))))))))
 
-(def exp  "./assets/day20/example.txt")
-(def exp2  "./assets/day20/example2.txt")
-(def inp  "./assets/day20/input.txt")
+(defn apply-cheating [{:keys [end dijk coords]} possible-cheats-fn]
+  (->> coords
+       (reduce (fn [acc k]
+                 (->> (possible-cheats-fn k)
+                      (valid-cheats dijk end)
+                      (reduce (fn [acc [k v]] (assoc acc k v)) acc)))
+               {})
+       (group-by second)
+       (map (fn [[k v]] [k (count v)]))
+       (filter #(>= (first %) 100))
+       (map second)
+       (c/sum)))
 
-(def mtx (c/to-matrix (slurp inp)))
-(def start (c/find-matrix-coord-of mtx \S))
-(def end (c/find-matrix-coord-of mtx \E))
-(def graph (c/matrix-to-graph mtx #{\S \E \.}))
-(def dijk (c/dijkstra-shortest graph start))
+(defn parse-input [file]
+  (let [mtx (c/to-matrix (slurp file))
+        start (c/find-matrix-coord-of mtx \S)
+        end (c/find-matrix-coord-of mtx \E)
+        graph (c/matrix-to-graph mtx #{\S \E \.})
+        dijk (c/dijkstra-shortest graph start)
+        coords (keys dijk)]
+    {:dijk dijk :coords coords :end end}))
 
-(let [ks (keys dijk)]
-  (->>  ks
-        (reduce (fn [acc k]
-                  (let [possible-cheats (->> ks
-                                             (filter #(not= k %))
-                                             (keep (fn [k1]
-                                                     (let [md (c/manhattan-distance k k1)]
-                                                       (when (<= md 20)
-                                                         [k md k1])))))
-                        valid-cheats (->> possible-cheats
-                                          (keep (fn [[c1 md c3]]
-                                                  (let [v1 (dijk c1)
-                                                        v3 (dijk c3)]
-                                                    (when (and (number? v1) (not (infinite? v1))
-                                                               (number? v3) (not (infinite? v3))
-                                                               (> v3 v1))
-                                                      (let [n (if (< (dijk end) v3)
-                                                                (- (dijk end)
-                                                                   (+ (- v3 (dijk end)) v1 md))
-                                                                (- v3 v1 md))]
-                                                        (when (pos? n) [#{c1 c3} n])))))))]
-                    (reduce (fn [acc [k v]]
-                              (assoc acc k v))
-                            acc
-                            valid-cheats)))
-                {})
-        (group-by second)
-        (map (fn [[k v]] [k (count v)]))
-       ;; (into (sorted-map))
-        (filter #(>= (first %) 100))
-        (map second)
-        (c/sum)
-     ;; 
-        ))
-;; is too low
-;; you need the tax cab distance here or manhathan distance between 2 points.
+(defn part1 [file]
+  (apply-cheating (parse-input file) possible-cheats))
+
+(defn part2 [file]
+  (let [input (parse-input file)
+        possible-cheats-fn #(possible-cheats % (:coords input))]
+    (apply-cheating input possible-cheats-fn )))
 
 
 
--- test/aoc24/day20_test.cljc
+++ test/aoc24/day20_test.cljc
@@ -3,10 +3,8 @@
             [aoc24.day20 :as day20]))
 
 (t/deftest part1
-  (t/testing "input" (t/is (= (day20/part1 "./assets/day20/input.txt") nil)))
-  (t/testing "example" (t/is (= (day20/part1 "./assets/day20/example.txt") nil))))
+  (t/testing "input" (t/is (= (day20/part1 "./assets/day20/input.txt") 1415))))
 
 (t/deftest part2
-  (t/testing "input" (t/is (= (day20/part2 "./assets/day20/input.txt") nil)))
-  (t/testing "example" (t/is (= (day20/part2 "./assets/day20/example.txt") nil))))
+  (t/testing "input" (t/is (= (day20/part2 "./assets/day20/input.txt") 1022577))))
 
