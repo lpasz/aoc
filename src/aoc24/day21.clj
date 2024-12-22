fa871154f63f2cfad4c9f55d2e518d00aea5d08c
Lucas Lopes Paszinski <lucaspaszinski@gmail.com>
2024-12-21 22:33:18 -0300

    Improvements on solution of day 21

--- src/aoc24/day21.clj
+++ src/aoc24/day21.clj
@@ -1,57 +1,63 @@
 (ns aoc24.day21
-  (:require [core :as c]))
+  (:require [core :as c]
+            [clojure.string :as s]))
 
-(def numpad (c/to-matrix "789\n456\n123\n#0A"))
+(def convert-numpad {\9 9 \8 8 \7 7 \6 6 \5 5 \4 4
+                     \3 3 \2 2 \1 1 \0 0 \# \# \A :a})
 
-(def directional-keypad (c/to-matrix "#^A\n<v>" identity {\# \# \^ :up \v :down \< :<- \> :-> \A :a}))
+(def numpad
+  (-> (s/join "\n" ["789"
+                    "456"
+                    "123"
+                    "#0A"])
+      (c/to-matrix identity convert-numpad)))
 
-(def dfs (memoize
-          (fn [mtx queue goal? result mmin]
-            (if (empty? queue) result
-                (let [[idx curr moves visited? :as entry] (first queue)
-                      queue (disj queue entry)]
-                  (cond (< mmin idx) result
-                        (goal? (mtx curr)) (recur mtx queue goal? (conj result moves) idx)
-                        :else   (let [directions (c/directions curr)
-                                      visited? (conj visited? curr)
-                                      unvisited-directions (->> directions
-                                                                (c/reject #(visited? (second %)))
-                                                                (c/reject #(#{\#} (mtx (second %))))
-                                                                (c/reject #(nil? (mtx (second %)))))]
-                                  (recur mtx
-                                         (reduce (fn [acc [dir coord]]
-                                                   (conj acc [(inc idx) coord (conj moves dir) visited?]))
-                                                 queue
-                                                 unvisited-directions)
-                                         goal?
-                                         result
-                                         mmin))))))))
+(def directional-keypad
+  (-> (s/join "\n" ["#^A"
+                    "<v>"])
+      (c/to-matrix identity {\# \# \^ :up \v :down \< :<- \> :-> \A :a})))
+
+(def dfs
+  (memoize
+   (fn
+     ([mtx start end] (dfs mtx
+                           (sorted-set [0 (c/find-matrix-coord-of mtx start) [] #{}])
+                           #{end}
+                           []
+                           c/inf))
+     ([mtx queue goal? result mmin]
+      (if (empty? queue)
+        result
+        (let [[idx curr moves visited? :as entry] (first queue)
+              queue (disj queue entry)]
+          (cond (< mmin idx) result
+                (goal? (mtx curr)) (recur mtx queue goal? (conj result moves) idx)
+                :else   (let [directions (c/directions curr)
+                              visited? (conj visited? curr)
+                              unvisited-directions (->> directions
+                                                        (c/reject #(visited? (second %)))
+                                                        (c/reject #(#{\#} (mtx (second %))))
+                                                        (c/reject #(nil? (mtx (second %)))))]
+                          (recur mtx
+                                 (reduce (fn [acc [dir coord]]
+                                           (conj acc [(inc idx) coord (conj moves dir) visited?]))
+                                         queue
+                                         unvisited-directions)
+                                 goal?
+                                 result
+                                 mmin)))))))))
 (def numpad-results
-  (let [nums "1234567890A"]
+  (let [nums [1 2 3 4 5 6 7 8 9 0 :a]]
     (->> (for [a nums b nums]
-           [[a b] (map #(concat % [:a])
-                       (dfs numpad
-                            (sorted-set [0
-                                         (c/find-matrix-coord-of numpad a)
-                                         []
-                                         #{}])
-                            #{b}
-                            []
-                            c/inf))])
-         (into (sorted-map)))))
+           [[a b] (->> (dfs numpad a b)
+                       (map #(concat % [:a])))])
+         (into {}))))
 
 (def dirpad-results
   (let [dirs [:up :-> :<- :down :a]]
     (->> (for [a dirs b dirs]
-           [[a b] (map #(concat % [:a])
-                       (dfs directional-keypad
-                            (sorted-set [0
-                                         (c/find-matrix-coord-of directional-keypad a)
-                                         []
-                                         #{}])
-                            #{b}
-                            []
-                            c/inf))])
+           [[a b] (->> (dfs directional-keypad a b)
+                       (map #(concat % [:a])))])
          (into (sorted-map)))))
 
 (defn find-paths [r1]
@@ -61,37 +67,40 @@
        (map vec)
        (map dirpad-results)))
 
-(def rr (memoize (fn [n p]
-                   (cond (c/one? n) (->> (find-paths p)
-                                        (map (fn [p]
-                                               (->> p
-                                                    (map count)
-                                                    (apply min))))
-                                        (c/sum))
-                         :else (->> (find-paths p)
-                                    (map (fn [p]
-                                           (->> p
-                                                (map #(rr (dec n) %))
-                                                (apply min))))
-                                    (c/sum))))))
+(def simulate-dir-robot
+  (memoize
+   (fn [robot-number p]
+     (if (c/one? robot-number)
+       (->> (find-paths p)
+            (map (fn [p]
+                   (->> p
+                        (map count)
+                        (apply min))))
+            (c/sum))
+       (->> (find-paths p)
+            (map (fn [p]
+                   (->> p
+                        (map #(simulate-dir-robot (dec robot-number) %))
+                        (apply min))))
+            (c/sum))))))
 
 (defn find-best-path [number robots-in-the-way]
   (->>  (str "A" number)
+        (map convert-numpad)
         (partition 2 1)
         (map vec)
         (map (fn [p]
                (->> p
-                    ;; num robot
-                    (numpad-results)
-                    (map #(rr robots-in-the-way %))
+                    (numpad-results) ;; num-pad-robot
+                    (map #(simulate-dir-robot robots-in-the-way %)) ;; dir-pad-robots
                     (apply min))))
         (c/sum)))
 
 (defn complexities-of-robots [file n]
   (let [txt (slurp file)
-        nums (c/extract-numbers txt)
-        steps (re-seq #"\d+\w" txt)]
-    (->> steps
+        input (re-seq #"\d+\w" txt)
+        nums (c/extract-numbers txt)]
+    (->> input
          (map #(find-best-path % n))
          (map * nums)
          (c/sum))))
@@ -101,5 +110,4 @@
 
 (defn part2 [file]
   (complexities-of-robots file 25))
-
 
--- test/aoc24/day21_test.cljc
+++ test/aoc24/day21_test.cljc
@@ -3,8 +3,10 @@
             [aoc24.day21 :as day21]))
 
 (t/deftest part1
-  (t/testing "input" (t/is (= (day21/part1 "./assets/day21/input.txt") 174124))))
+  (t/testing "input" (t/is (= (day21/part1 "./assets/day21/input.txt") 174124)))
+  (t/testing "example" (t/is (= (day21/part1 "./assets/day21/example.txt") 126384))))
 
 (t/deftest part2
-  (t/testing "input" (t/is (= (day21/part2 "./assets/day21/input.txt") 216668579770346))))
+  (t/testing "input" (t/is (= (day21/part2 "./assets/day21/input.txt") 216668579770346)))
+  (t/testing "example" (t/is (= (day21/part2 "./assets/day21/example.txt") 154115708116294))))
 
