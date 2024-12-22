4b33229d804717c7d5a5c5df6cb7851b01084d8f
Lucas Lopes Paszinski <lucaspaszinski@gmail.com>
2024-12-22 10:46:40 -0300

    Day 22 Part 2 + Tests

--- src/aoc24/day22.clj
+++ src/aoc24/day22.clj
@@ -1,19 +1,11 @@
 (ns aoc24.day22
   (:require [core :as c]))
 
-(defn part1 [file] :not-implemented)
-
-(defn part2 [file] :not-implemented)
-
 (defn prune [sn]
   (mod sn 16777216))
 
-;(prune 100000000) ;; 16113920
-
 (defn mix [sn n]
   (bit-xor sn n))
-
-;(mix 42 15) ;; 37
 
 (defn step1 [sn]
   (prune (mix sn (* sn 64))))
@@ -24,20 +16,62 @@
 (defn step3 [sn]
   (prune (mix sn (* sn 2048))))
 
-(defn next-secret-number
-  ([n sn] (loop [n n sn [sn]]
-            (if (zero? n)
-              sn
-              (recur (dec n) (conj sn (next-secret-number (peek sn)))))))
-  ([sn]
-   (->> sn
-        (step1)
-        (step2)
-        (step3))))
+(def next-secret-number
+  (memoize
+   (fn
+     ([n sn]
+      (loop [n n sn sn prices [(mod sn 10)]]
+        (if (zero? n)
+          [sn prices]
+          (let [secret-number (next-secret-number sn)]
+            (recur (dec n) secret-number (conj prices (mod secret-number 10)))))))
+     ([sn]
+      (->> sn
+           (step1)
+           (step2)
+           (step3))))))
 
 (defn part1 [file]
   (->> (slurp file)
        (c/extract-numbers)
        (map #(next-secret-number 2000 %))
-       (map peek)
+       (map first)
        (c/sum)))
+
+(defn get-prices [n nums]
+  (->> nums
+       (map #(next-secret-number n %))
+       (map second)))
+
+(defn get-price-changes [prices]
+  (->> prices
+       (map #(partition 2 1 %))
+       (map #(map (fn [[p1 p2]] [(- p2 p1) p2]) %))))
+
+(defn get-price-after-4-changes [prices]
+  (->> prices
+       (map #(partition 4 1 %))
+       (map #(map (fn [[[c1 _] [c2 _] [c3 _] [c4 price]]] [price [c1 c2 c3 c4]]) %))
+       (map #(reduce (fn [acc [price changes]]
+                       (if (acc changes)
+                         acc
+                         (assoc acc changes price))) {} %))
+       (into [])))
+
+(defn find-most-bananas-across-all-vendors [vendor-prices-after-4-changes]
+  (->> vendor-prices-after-4-changes
+       (c/flatten-once)
+       (group-by first)
+       (map (fn [[k v]] [k (->> (map second v) (c/sum))]))
+       (sort-by second >)
+       (first)
+       (second)))
+
+(defn part2 [file]
+  (->> (slurp file)
+       (c/extract-numbers)
+       (get-prices 2000)
+       (get-price-changes)
+       (get-price-after-4-changes)
+       (find-most-bananas-across-all-vendors)))
+
--- test/aoc24/day22_test.cljc
+++ test/aoc24/day22_test.cljc
@@ -3,10 +3,10 @@
             [aoc24.day22 :as day22]))
 
 (t/deftest part1
-  (t/testing "input" (t/is (= (day22/part1 "./assets/day22/input.txt") nil)))
-  (t/testing "example" (t/is (= (day22/part1 "./assets/day22/example.txt") nil))))
+  (t/testing "input" (t/is (= (day22/part1 "./assets/day22/input.txt") 13234715490)))
+  (t/testing "example" (t/is (= (day22/part1 "./assets/day22/example.txt") 37327623))))
 
 (t/deftest part2
-  (t/testing "input" (t/is (= (day22/part2 "./assets/day22/input.txt") nil)))
-  (t/testing "example" (t/is (= (day22/part2 "./assets/day22/example.txt") nil))))
+  (t/testing "input" (t/is (= (day22/part2 "./assets/day22/input.txt") 1490)))
+  (t/testing "example2" (t/is (= (day22/part2 "./assets/day22/example2.txt") 23))))
 
