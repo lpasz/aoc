bc3705d766c5e041c2572ae51aa8d98e15b85d87
Lucas Lopes Paszinski <lucaspaszinski@gmail.com>
2024-12-23 11:38:50 -0300

    Part 1 and Part 2 + Test, still need to reimplement the bk myself.

--- src/aoc24/day23.clj
+++ src/aoc24/day23.clj
@@ -1,47 +1,75 @@
 (ns aoc24.day23
   (:require [core :as c]
-            [clojure.string :as s]))
+            [clojure.string :as s]
+            [clojure.set :as set]))
 
-(defn part1 [file] :not-implemented)
+(defn parse-input [file]
+  (let [node-connections (->> (re-seq #"\w+" (c/get-input file))
+                              (partition 2))
+        neighbours (->> node-connections
+                        (reduce (fn [acc [p1 p2]]
+                                  (->
+                                   acc
+                                   (update p1 (fn [conn] (conj (or conn #{}) p2)))
+                                   (update p2 (fn [conn] (conj (or conn #{}) p1)))))
+                                {}))]
+    {:nodes (set (map first node-connections))
+     :neighbours neighbours}))
 
-(defn part2 [file] :not-implemented)
+(defn part1 [file]
+  (let [{:keys [nodes neighbours]} (parse-input file)]
+    (->> (for [i nodes]
+           (for [j (neighbours  i)]
+             (for [k (neighbours j)
+                   :when (and (not= i j)
+                              (not= j k)
+                              (not= k j))]
+               (let [si (neighbours i)
+                     sj (neighbours j)
+                     sk (neighbours k)]
+                 (when (and (si j) (si k)
+                            (sj i) (sj k)
+                            (sk i) (sk j))
+                   (set [i j k]))))))
+         (flatten)
+         (filter #(= (count %) 3))
+         (c/reject nil?)
+         (set)
+         (filter (partial some #(s/starts-with? % "t")))
+         (count))))
 
-(def exp (->> (re-seq #"\w+" (c/get-input "input.txt"))
-              (partition 2)))
+(defn vector-intersection
+  "Finds the intersection between two vectors.
+  Assumes no duplicate elements in either vector."
+  [vec-1 vec-2]
+  (apply vector (set/intersection (set vec-1) (set vec-2))))
 
-(def sexp (set (map first exp)))
+(defn bron-kerbosch
+  "Performs the Bron-Kerbosch iterative algorithm."
+  ([nodes neighbours] (bron-kerbosch [] nodes [] neighbours))
+  ([r p x neighbours]
+   (if (and (empty? p) (empty? x))
+     [(set r)]
+     (loop [p p
+            x x
+            result []]
+       (if (empty? p)
+         result
+         (let [v (first p)
+               nv (neighbours v)
+               result (into result
+                            (bron-kerbosch (cons v r)
+                                           (vector-intersection p nv)
+                                           (vector-intersection x nv)
+                                           neighbours))
+               p (rest p)
+               x (cons v x)]
+           (recur p x result)))))))
 
-(def mp (->> exp
-             (reduce (fn [acc [p1 p2]]
-                       (->
-                        acc
-                        (update p1 (fn [conn] (conj (or conn #{}) p2)))
-                        (update p2 (fn [conn] (conj (or conn #{}) p1)))))
-                     {})))
-
-
-(->> (for [i (map first exp)]
-       (for [j (mp i)]
-         (for [k (mp j)
-               :when (and (not= i j)
-                          (not= j k)
-                          (not= k j))]
-           (let [si (mp i)
-                 sj (mp j)
-                 sk (mp k)]
-             (when (and (si j) (si k)
-                        (sj i) (sj k)
-                        (sk i) (sk j))
-               (set [i j k])))
-
-;;
-           )))
-     (flatten)
-     (filter #(= (count %) 3))
-     (c/reject nil?)
-     (set)
-     (filter (partial some #(s/starts-with? % "t")))
-     (count)
-     ;;
-     )
-
+(defn part2 [file]
+  (let [{:keys [nodes neighbours]} (parse-input file)]
+    (->> (bron-kerbosch nodes neighbours)
+         (sort-by count >)
+         (first)
+         (sort)
+         (s/join ","))))
--- test/aoc24/day23_test.cljc
+++ test/aoc24/day23_test.cljc
@@ -3,10 +3,10 @@
             [aoc24.day23 :as day23]))
 
 (t/deftest part1
-  (t/testing "input" (t/is (= (day23/part1 "input.txt") nil)))
-  (t/testing "example" (t/is (= (day23/part1 "example.txt") nil))))
+  (t/testing "input" (t/is (= (day23/part1 "input.txt") 1352)))
+  (t/testing "example" (t/is (= (day23/part1 "example.txt") 7))))
 
 (t/deftest part2
-  (t/testing "input" (t/is (= (day23/part2 "input.txt") nil)))
-  (t/testing "example" (t/is (= (day23/part2 "example.txt") nil))))
+  (t/testing "input" (t/is (= (day23/part2 "input.txt") "dm,do,fr,gf,gh,gy,iq,jb,kt,on,rg,xf,ze")))
+  (t/testing "example" (t/is (= (day23/part2 "example.txt") "co,de,ka,ta"))))
 
