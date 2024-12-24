aa4e5653bab4e66699684b5cd00477edde17e50d
Lucas Lopes Paszinski <lucaspaszinski@gmail.com>
2024-12-24 16:20:04 -0300

    Save

--- src/aoc24/day24.clj
+++ src/aoc24/day24.clj
@@ -2,8 +2,6 @@
   (:require [core :as c]
             [clojure.string :as s]))
 
-(defn part1 [file] :not-implemented)
-
 (defn part2 [file] :not-implemented)
 
 (defn parse-input [file]
@@ -17,7 +15,6 @@
                (map next)
                (map #(map keyword (c/reject nil? %))))]
     {:values v :operations t}))
-
 
 (defn produce-wire-result [file]
   (let [{:keys [values operations]} (parse-input file)]
@@ -40,24 +37,83 @@
                                                   [values (conj operations itm)])))) [values []] operations)]
           (recur values operations))))))
 
-(->> (produce-wire-result "input.txt")
-     (first)
-     ((fn [m]
-        (let [z-nums (->> (keys m)
-                          (map str)
-                          (filter #(s/starts-with? % ":z"))
-                          (map #(re-seq #"\d+" %))
-                          (map first)
-                          (sort)
-                          (c/insp))]
-          (loop [z-nums z-nums r '()]
-            (if (empty? z-nums)
-              r
-              (let [z-num (first z-nums)
-                    z-rest (rest z-nums)]
-                (recur z-rest (conj r (m (keyword (str "z" z-num)))))))))))
-     (apply str)
-     (#(Long/parseLong % 2)))
+(defn part1 [file]
+  (->> (produce-wire-result file)
+       (first)
+       ((fn [m]
+          (let [z-nums (->> (keys m)
+                            (map str)
+                            (filter #(s/starts-with? % ":z"))
+                            (map #(re-seq #"\d+" %))
+                            (map first)
+                            (sort))]
+            (loop [z-nums z-nums r '()]
+              (if (empty? z-nums)
+                r
+                (let [z-num (first z-nums)
+                      z-rest (rest z-nums)]
+                  (recur z-rest (conj r (m (keyword (str "z" z-num)))))))))))
+       (apply str)
+       (#(Long/parseLong % 2))))
 
+(defn make-key [letter n]
+  (keyword (str letter (format "%02d" n))))
 
+(def != not=)
 
+(defn verify-direct-carry [wire n formula]
+  (c/insp :verify-direct-carry  [wire n])
+  (let [[o x y] (formula wire)]
+    (c/insp :verify-direct-carry  [o x y])
+    (if (!= o :AND) false
+        (= (sort [x y]) (map #(make-key % n) ["x" "y"])))))
+
+(defn verify-intermediate-xor [wire n formula]
+  (c/insp :verify-intermediate-xor [wire n])
+  (let [[o x y] (formula wire)]
+    (c/insp :verify-intermediate-xor [o x y])
+    (if (!= o :XOR) false
+        (= (sort [x y]) (map #(make-key % n) ["x" "y"])))))
+
+(defn verify-carry-bit [wire n formula]
+  (c/insp :verify-carry-bit [wire n])
+  (let [[o x y] (formula wire)]
+    (c/insp :verify-carry-bit [o x y])
+    (cond (and (c/one? n) (not= o :AND)) false
+          (c/one? n) (= (sort [x y]) (map #(make-key % (dec n)) ["x" "y"]))
+          (not= o :OR) false
+          :else (or (and (verify-direct-carry x (dec n) formula)
+                         (verify-recarry y (dec n) formula))
+                    (and (verify-direct-carry y (dec n) formula)
+                         (verify-recarry x (dec n) formula))))))
+
+(defn verify-recarry [wire n formula]
+  (c/insp :verify-recarry [wire n])
+  (let [[o x y] (formula wire)]
+    (if (!= o :AND) false
+        (or (and (verify-intermediate-xor x n formula)
+                 (verify-carry-bit y n formula))
+            (and (verify-intermediate-xor  y n formula)
+                 (verify-carry-bit x n formula))))))
+
+(defn verify-z [wire n formula]
+  (c/insp :verify-z [wire n])
+  (let [[o x y] (formula wire)]
+    (c/insp :verify-z [o x y])
+    (cond (not= o :XOR) false
+          (zero? n) (= (sort [x y]) [:x00 :y00])
+          :else (or (and (verify-intermediate-xor  x n formula)
+                         (verify-carry-bit y n formula))
+                    (and (verify-intermediate-xor y n formula)
+                         (verify-carry-bit x n formula))))))
+
+(let [{:keys [operations]} (parse-input "input.txt")
+      formulas (->> operations (map (fn [[x o y z]] [z [o x y]])) (into (sorted-map)))
+      n 1]
+  (loop [n (range 0 46)]
+    (if (verify-z (make-key "z" (first n)) (first n) formulas)
+      (recur (rest n))
+      (first n))))
+
+;; z16 <-> hmk
+;; fhp <-> z20
--- test/aoc24/day24_test.cljc
+++ test/aoc24/day24_test.cljc
@@ -3,8 +3,8 @@
             [aoc24.day24 :as day24]))
 
 (t/deftest part1
-  (t/testing "input" (t/is (= (day24/part1 "input.txt") nil)))
-  (t/testing "example" (t/is (= (day24/part1 "example.txt") nil))))
+  (t/testing "input" (t/is (= (day24/part1 "input.txt") 66055249060558)))
+  (t/testing "example" (t/is (= (day24/part1 "example.txt") 2024))))
 
 (t/deftest part2
   (t/testing "input" (t/is (= (day24/part2 "input.txt") nil)))
