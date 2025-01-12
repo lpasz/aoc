a55d69111560d11ac0219b60d5e7ec7e6a1b0fd3
Lucas Lopes Paszinski <lucaspaszinski@gmail.com>
2025-01-11 21:59:56 -0300

    Save

--- src/aoc15/day13.clj
+++ src/aoc15/day13.clj
@@ -1,7 +1,7 @@
 (ns aoc15.day13
   (:require [core :as c]))
 
-(defn parse-input [file]
+(defn parse-input [file additional]
   (->> (c/get-input file)
        (re-seq #"(\w+) would (lose|gain) (\d+) happiness units by sitting next to (\w+)\.")
        (reduce (fn [acc [_ person1 gain-lose quantity person2]]
@@ -10,7 +10,19 @@
                            (assoc acc person2
                                   (if (= "gain" gain-lose)
                                     (parse-long quantity)
-                                    (- (parse-long quantity))))))) {})))
+                                    (- (parse-long quantity))))))) {})
+       (c/then [graph]
+               (->> (keys graph)
+                    (reduce (fn [acc k1]
+                              (let [happiness (graph k1)
+                                    happiness (reduce (fn [acc [k2 v]]
+                                                        (assoc acc
+                                                               k2
+                                                               (+ ((graph k2) k1) v)))
+                                                      additional
+                                                      happiness)]
+                                (assoc acc k1 happiness)))
+                            {})))))
 
 (defn cyclic-distances
   ([graph start]
@@ -22,39 +34,17 @@
        [(+ acc (get-in graph [curr end]))]
        (mapcat (fn [[k v]]
                  (cyclic-distances graph
-                                  k
-                                  end
-                                  (conj visited curr)
-                                  (+ v acc))) i)))))
+                                   k
+                                   end
+                                   (conj visited curr)
+                                   (+ v acc))) i)))))
 
 (defn part1 [file]
-  (let [graph (parse-input file)
-        ngraph (->> (keys graph)
-                    (reduce (fn [acc k1]
-                              (let [happiness (graph k1)
-                                    happiness (reduce (fn [acc [k2 v]]
-                                                        (assoc acc
-                                                               k2
-                                                               (+ ((graph k2) k1) v)))
-                                                      {}
-                                                      happiness)]
-                                (assoc acc k1 happiness)))
-                            {}))]
-    (apply max (cyclic-distances ngraph "Alice"))))
+  (let [graph (parse-input file {})]
+    (apply max (cyclic-distances graph "Alice"))))
 
 (defn part2 [file]
-  (let [graph (parse-input file)
-        ngraph (->> (keys graph)
-                    (reduce (fn [acc k1]
-                              (let [happiness (graph k1)
-                                    happiness (reduce (fn [acc [k2 v]]
-                                                        (assoc acc
-                                                               k2
-                                                               (+ ((graph k2) k1) v)))
-                                                      {}
-                                                      happiness)]
-                                (assoc acc k1 (assoc happiness "Myself" 0))))
-                            {}))
-        myself (->> (keys ngraph) (map #(vector % 0)) (into {}))
-        ngraph (-> ngraph (assoc "Myself" myself))]
-    (apply max (cyclic-distances ngraph "Alice"))))
+  (let [graph (parse-input file {"Myself" 0})
+        myself (->> (keys graph) (map #(vector % 0)) (into {}))
+        graph (assoc graph "Myself" myself)]
+    (apply max (cyclic-distances graph "Alice"))))
