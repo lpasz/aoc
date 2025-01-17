1f22eb03a7a1ee67b83f4f7cd92b37f90759f341
Lucas Lopes Paszinski <lucaspaszinski@gmail.com>
2025-01-16 22:03:41 -0300

    Aoc 15 Day 17 P1 & P2
    It's just a binary split each time, one goes in, other one goes without, nothing fancy at all

--- src/aoc15/day17.clj
+++ src/aoc15/day17.clj
@@ -1,2 +1,26 @@
 (ns aoc15.day17
   (:require [core :as c]))
+
+(defn binary-search
+  ([total containers] (binary-search 0 0 total containers))
+  ([lts cnt total [head & tail :as containers]]
+   (cond (= lts total) [cnt]
+         (> lts total) []
+         (empty? containers) []
+         :else (concat (binary-search (+ lts head) (inc cnt) total tail)
+                       (binary-search lts cnt total tail)))))
+
+(defn part1 [file]
+  (->> (c/get-input file)
+       (c/extract-numbers)
+       (binary-search 150)
+       (count)))
+
+(defn part2 [file]
+  (->> (c/get-input file)
+       (c/extract-numbers)
+       (binary-search 150)
+       (sort)
+       (partition-by identity)
+       (first)
+       (count)))
