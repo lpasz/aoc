e830eb659cf5735ca3a1c920c31967c2690dae77
Lucas Lopes Paszinski <lucaspaszinski@gmail.com>
2025-12-03 09:36:11 -0300

    P3 Working

--- src/aoc25/day03.clj
+++ src/aoc25/day03.clj
@@ -10,36 +10,17 @@
                (fn [idx itm] [idx (c/parse-int itm)])
                (re-seq #"\d" line))))))
 
-(parse-input "input.txt")
-
-(defn part1 [file]
-  (let [items (parse-input file)]
-    (->> items
-         (map (fn [line]
-                (for [[idx1 i1] line
-                      [idx2 i2] line
-                      :when (< idx1 idx2)]
-                  (c/parse-int (str i1 i2)))))
-         (map #(apply max %))
-         (c/sum))))
-
-(part1 "input.txt")  ; 357
-
-(def comb
-  (->> (c/inc-range 0 11)
-       (c/rotations)
-       (map #(drop 1 %))
-       (map sort)))
 
 (defn next-combinations [curr-bat next-bat]
   (->> (for [idx (range 0 (count curr-bat))]
          (conj (c/remove-at idx curr-bat) next-bat))
        (c/then [coll] (conj coll curr-bat))
-       (map (fn [coll] [coll (c/parse-int (apply str coll))]))
+       (map (fn [coll] [coll (parse-long (apply str coll))]))
        (sort-by second >)
        (ffirst)))
 
 (defn max-jolt [batteries num-of-batteries]
+  (c/insp batteries)
   (let [batteries (map (fn [[_ jolts]] jolts) batteries)
         curr-bat (vec (take num-of-batteries batteries))
         rem-bats (drop num-of-batteries batteries)]
@@ -53,12 +34,14 @@
            (next-combinations curr-bat head)
            tail))))))
 
-(defn part2 [file]
+(defn max-jolts file num-of-batteries
   (let [lines (parse-input file)]
     (->> (for [line lines]
-           (max-jolt line 12))
+           (max-jolt line num-of-batteries))
          (map #(apply str %))
-         (map c/parse-int)
+         (map parse-long)
          (c/sum))))
 
-(part2 "example.txt")
+(defn part1 [file] (max-jolts file 2))
+(defn part2 [file] (max-jolts file 12))
+
