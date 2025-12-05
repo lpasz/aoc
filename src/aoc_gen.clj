fe7752cabd451210cbe312aefda7163530757470
Lucas Lopes Paszinski <lucaspaszinski@gmail.com>
2025-12-05 19:57:40 -0300

    Improve aoc_gen

--- src/aoc_gen.clj
+++ src/aoc_gen.clj
@@ -4,9 +4,7 @@
             [clojure.string :as str])
   (:gen-class))
 
-;; --- Templates de Arquivo (Corrigidos conforme sua especificação) ---
-
-(defn- clj-file [y-suffix day-pad] ; Usando y-suffix e day-pad internamente
+(defn- clj-file [y-suffix day-pad]
   (str "(ns aoc" y-suffix ".day" day-pad "
   (:require [core :as c]
             [clojure.string :as str]))
@@ -14,7 +12,7 @@
 (defn part1 [file-path] :part1)
 (defn part2 [file-path] :part2)"))
 
-(defn- cljc-file [y-suffix day-pad] ; Usando y-suffix e day-pad internamente
+(defn- cljc-file [y-suffix day-pad]
   (str "(ns aoc" y-suffix ".day" day-pad "-test
   (:require [clojure.test :as t]
             [aoc" y-suffix ".day" day-pad " :as day" day-pad "]))
@@ -31,20 +29,12 @@
   (t/testing \"Real Input\"
     (t/is (= :boom (day" day-pad "/part2 \"input.txt\")))))"))
 
-;; --- Lógica de Formatação e Geração ---
-
 (defn- format-aoc-inputs [year day]
-  "Converte entrada flexível (string/número, 2025/25, 4/04) em strings de 2 dígitos."
   (let [y (if (string? year) (Long/parseLong year) year)
         d (if (string? day) (Long/parseLong day) day)
-
-        ;; Pega o sufixo de 2 dígitos do ano (ex: 2025 -> "25", 25 -> "25")
         y-suffix-long (mod y 100)
         y-suffix (format "%02d" y-suffix-long)
-
-        ;; Garante 2 dígitos para o dia (ex: 4 -> "04")
         day-pad (format "%02d" d)]
-
     {:year-full y :y-suffix y-suffix :day-pad day-pad}))
 
 (defn- create-file [filepath content]
@@ -54,39 +44,30 @@
     (str "Criado: " filepath)))
 
 (defn get-input-from-aoc [year day]
-  (->> (http-client/get (str "https://adventofcode.com/20" year "/day/" (parse-long day) "/input")
-                        {:headers {"Cookie" (str/trim (slurp ".aoc-session"))}})
+  (->> (str "https://adventofcode.com/20" year "/day/" (parse-long day) "/input")
+       (http-client/get {:headers {"Cookie" (str/trim (slurp ".aoc-session"))}})
        (deref)))
 
 (defn- setup-day [year day]
   (let [{:keys [y-suffix day-pad]} (format-aoc-inputs year day)
-
         mod-path (str "src/aoc" y-suffix "/day" day-pad ".clj")
         test-path (str "test/aoc" y-suffix "/day" day-pad "_test.clj")
         asset-dir (str "assets/aoc" y-suffix "/day" day-pad "/")
         input-path (str asset-dir "input.txt")
         example-path (str asset-dir "example.txt")]
-
     (println "--- Gerando Arquivos ---")
-    ;; Chamando as funções de template renomeadas: clj-file e cljc-file
     (println (create-file mod-path (clj-file y-suffix day-pad)))
     (println (create-file test-path (cljc-file y-suffix day-pad)))
-
-    ;; Garante que a pasta de assets exista e cria arquivos placeholders
     (io/make-parents input-path)
-
     (let [response (get-input-from-aoc year day)]
       (when (= 200 (:status response))
+        (println (str "Criado: " input-path))
         (spit input-path (:body response))))
-
-    (spit example-path "") ; Arquivo de exemplo vazio
-    (println (str "Criado assets/dir: " input-path ", " example-path))
-
+    (spit example-path "")
+    (println (str "Criado: " example-path))
     (println (str "\n✨ Day " day-pad " (20" year ") pronto!")))
 
   (shutdown-agents))
-
-;; --- Função Principal (Entry Point) ---
 
 (defn -main
   "Função principal para ser executada via CLI.
